d84af7a46c6d2f794213e9ce656208e9ac404f89
Sergey <metrolog_omsk@mail.ru>
2022-07-01 20:33:01 +0500

    Решил задачу № 41

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,24 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел
 // Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+Console.Clear();
+Console.WriteLine("**********************************************");
+Console.WriteLine("Введите число ограничения ввода:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("**********************************************");
+Console.WriteLine($"Было введено {CountInput(number)} числа (-ел) > 0");
+Console.WriteLine("**********************************************");
+
+int CountInput(int num)
+{
+    Console.WriteLine("Введите любые целые числа:");
+    int count = 0;
+    int number = 0;
+    for (int i = 0; i < num; i++)
+    {
+        number = Convert.ToInt32(Console.ReadLine());
+        if (number > 0) count++;
+    }
+    return count;
+}
